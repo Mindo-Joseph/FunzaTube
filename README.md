cfc813960b552b23ef5f4d3dbdbacf8fb54881a8
Joseph Mindo <mindo.joseph.mj@gmail.com>
2020-10-19 14:42:57 +0300

    Remove python files

--- README.md
+++
@@ -1,2 +0,0 @@
-# FunzaTube
-A tech tutorial site
--- funzatube/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/joe/Desktop/Projects/FunzaTube/funzatube/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
